cd1caf71e10ef1267fd48bd9ac5e44e383604523
Jonathan Sedrez <jonathansedrez@gmail.com>
2025-11-24 22:07:44 -0300

    feat: add heartbeat to server

--- server/index.js
+++ server/index.js
@@ -2,11 +2,24 @@ import { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
+setInterval(() => {
+  wss.clients.forEach((ws) => {
+    if (!ws.isAlive) return ws.terminate();
+    ws.isAlive = false;
+    ws.ping();
+  });
+}, 30000);
+
 wss.on("connection", (ws) => {
   ws.send("Welcome!");
+
+  ws.isAlive = true;
+  ws.on("pong", () => (ws.isAlive = true));
+
   ws.on("message", (msg) => {
     for (const client of wss.clients) {
       if (client.readyState === 1) {
+        console.log("client", client);
         client.send(msg.toString());
       }
     }
