256988f327b120d8a24453ab4e8734a25ceb084b
Jonathan Sedrez <jonathansedrez@gmail.com>
2025-11-24 22:14:04 -0300

    feat: add client connection

--- client/app.js
+++ client/app.js
@@ -1,18 +1,28 @@
-const socket = new WebSocket("ws://localhost:8080");
+let socket;
 const messageInput = document.getElementById("messageInput");
 const sendBtn = document.getElementById("sendBtn");
 const messagesDiv = document.getElementById("messages");
 
-socket.addEventListener("open", () => {
-  console.log("Connected to server");
-});
+function connect() {
+  socket = new WebSocket("ws://localhost:8080");
 
-socket.addEventListener("message", (event) => {
-  console.log("Message from server ", event.data);
-  const p = document.createElement("p");
-  p.textContent = "Server: " + event.data;
-  messagesDiv.appendChild(p);
-});
+  socket.addEventListener("open", () => {
+    console.log("Connected to server");
+  });
+  socket.addEventListener("message", (event) => {
+    const p = document.createElement("p");
+    p.textContent = "Server: " + event.data;
+    messagesDiv.appendChild(p);
+  });
+  socket.addEventListener("error", (error) => {
+    console.error("WebSocket error:", error);
+  });
+  socket.addEventListener("close", () => {
+    setTimeout(connect, 1000);
+  });
+}
+
+connect();
 
 sendBtn.addEventListener("click", () => {
   const message = messageInput.value;
